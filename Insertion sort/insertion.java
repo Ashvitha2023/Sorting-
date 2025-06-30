63acbc5b83dfc5bc02f020ab9ff81b0c51d939c9
Ashvitha2023 <ashvithakesavan2005@gmail.com>
2025-06-30 09:27:36 +0530

    Added

---
+++ Insertion sort/insertion.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class insertion {
+    static void sort(int[]arr){
+        for(int i=0;i<arr.length-1;i++){
+            for(int j=i+1;j>0;j--){
+                if(arr[j]<arr[j-1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j-1];
+                    arr[j-1]=temp;
+                }
+                else break;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int size=sc.nextInt();
+        int []arr=new int[size];
+        for(int i=0;i<size;i++){
+            arr[i]=sc.nextInt();
+        }
+        sort(arr);
+        System.out.print(Arrays.toString(arr));
+    }
+}
